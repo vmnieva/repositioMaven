f53947c6b6d70f618374f427be1e149c3f02a337
Victor Manuel Nieva Sanchez <vmanuelnieva@gmail.com>
2024-04-25 09:35:43 +0200

    Add files via upload

---
+++ ejercicioMaven/src/main/java/victor/ejercicioMaven/EjercicioMavenApplication.java
@@ -0,0 +1,13 @@
+package victor.ejercicioMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioMavenApplication.class, args);
+	}
+
+}
---
+++ ejercicioMaven/src/test/java/victor/ejercicioMaven/EjercicioMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.ejercicioMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
